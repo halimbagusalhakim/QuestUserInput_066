66b9a7f21858b93c6eb9ab634c023603bf4089d1
halimbagusalhakim <halsat132@gmail.com>
2025-10-30 11:46:33 +0700

    menambahkan kolom dengan modifier

--- app/src/main/java/com/example/praktikum4/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum4/TeksEdit.kt
@@ -1,11 +1,15 @@
 package com.example.praktikum4
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 
 @Composable
@@ -22,5 +26,12 @@ fun FormatDataDiri(modifier: Modifier){
     var jenis by remember {mutableStateOf("")}
 
     val gender:List<String> = listOf("Laki-Laki", "Perempuan")
+
+    Column(modifier = Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = alignment.CenterHorizontally){
+
+    }
+        )
 
 }
