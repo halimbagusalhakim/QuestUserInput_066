5f8794be392c667d3cd244988ab9c6d8b8e06e90
halimbagusalhakim <halsat132@gmail.com>
2025-10-30 11:36:25 +0700

    insialisasi teksEdit dan membuat fungsi FormatDataDiri

---
+++ app/src/main/java/com/example/praktikum4/TeksEdit.kt
@@ -0,0 +1,11 @@
+package com.example.praktikum4
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+
+fun FormatDataDiri(modifier: Modifier){
+    //
+}
