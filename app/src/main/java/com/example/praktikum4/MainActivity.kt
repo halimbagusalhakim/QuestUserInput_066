df96792bbfd7bdc487ae590e2b592e0d513ef158
halimbagusalhakim <halsat132@gmail.com>
2025-10-30 12:41:41 +0700

    memamnggil fungsi formatDataDIri ke main activty

--- app/src/main/java/com/example/praktikum4/MainActivity.kt
+++ app/src/main/java/com/example/praktikum4/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Praktikum4Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    FormatDataDiri(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
